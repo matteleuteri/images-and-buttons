3ed8265931ba3185fa09323409e8a0b24f2b1f68
“Matthew <meleuteri2@gmail.com>
2022-10-15 14:59:54 -0400

    formatting fixes.

--- src/index.js
+++ src/index.js
@@ -6,63 +6,62 @@ import Square from './Square';
 
 
 class MainContainer extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      filters: ["filter 1", "filter 2", "filter 3"],
-    };
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            filters: ["filter 1", "filter 2", "filter 3"],
+        };
+    }
 
-  handleClick(i) {
-    console.log(i);
-  }
+    handleClick(i) {
+        console.log(i);
+    }
 
-  renderButton(i) {
-    return (
-      <Square
-        value={this.state.filters[i]}
-        onClick={() => this.handleClick(i)}
-      />
-    );
-  }
+    renderButton(i) {
+        return (
+            <Square
+                value={this.state.filters[i]}
+                onClick={() => this.handleClick(i)}
+            />
+        );
+    }
 
-  renderImage() {
-    return (
-      <img src='logo192.png' alt='not found'></img>
-    )
-  }
+    renderImage() {
+        return (
+            <img src='logo192.png' alt='not found'></img>
+        )
+    }
 
-  render() {
-    return (
-      <div>
-        <div className="row">
-          {this.renderButton(0)}{this.renderButton(1)}{this.renderButton(2)}
-        </div>
-        <div className="imageContainer">
-          {
-            <Circle />
-            //this.renderImage()
-          }
-        </div>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <div className="row">
+                    {this.renderButton(0)}{this.renderButton(1)}{this.renderButton(2)}
+                </div>
+                <div className="imageContainer">
+                {
+                    <Circle />
+                }
+                </div>
+            </div>
+        );
+    }
 }
 
 class App extends React.Component {
-  render() {
-    return (
-      <div className="app">
-        <div className="board">
-          <MainContainer />
-        </div>
-        <div className="info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
-        </div>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="app">
+                <div className="board">
+                    <MainContainer />
+                </div>
+                <div className="info">
+                    <div>{/* status */}</div>
+                    <ol>{/* TODO */}</ol>
+                </div>
+            </div>
+        );
+    }
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
