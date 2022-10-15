f0a97353428eac48bac10de3f00c088df8a8cea1
“Matthew <meleuteri2@gmail.com>
2022-10-14 21:29:30 -0400

    initial commit

---
+++ src/index.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+class Square extends React.Component {
+  render() {
+    return (
+      <button
+        className="square"
+        onClick={() => this.props.onClick()}
+      >
+      {this.props.value}
+      </button>
+    );
+  }
+}
+
+class MainContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      filters: ["filter 1", "filter 2", "filter 3"],
+    };
+  }
+
+  handleClick(i) {
+    console.log(i);
+  }
+
+  renderButton(i) {
+    return (
+      <Square
+        value={this.state.filters[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
+  }
+
+  renderImage() {
+    return (
+      <img src='logo192.png' alt='not found'></img>
+    )
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="row">
+          {this.renderButton(0)}{this.renderButton(1)}{this.renderButton(2)}
+        </div>
+        <div className="imageContainer">
+          {this.renderImage()}
+        </div>
+      </div>
+    );
+  }
+}
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="app">
+        <div className="board">
+          <MainContainer />
+        </div>
+        <div className="info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App/>);
